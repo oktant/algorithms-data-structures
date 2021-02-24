42dc2a4c546e476719a98cdf1bb6cbe1518f5e3a
Oktay Alizada <oktay.alizada@protonmail.com>
2021-02-25 00:54:25 +0100

    Update FibonacciSumLastDigit with shorter solution

--- src/FibonacciSumLastDigit.java
+++ src/FibonacciSumLastDigit.java
@@ -1,46 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class FibonacciSumLastDigit {
-    static List<Integer> arrayList = new ArrayList<>();
-
-
     private static long getFibonacciSumNaive(long n) {
-        getFibonacciHugeNaive(n);
-
-        long a=sum((int)n%60);
-        long full=a/60;
-        long k=0;
-        if(full>0) {
-            k=full*(arrayList.stream().parallel().mapToInt(Integer::intValue).sum()-1);
+        n = (n+2)%60;
+        long[] fibNumbers =new long[(int) (n+1)];
+        fibNumbers[0]=0;
+        fibNumbers[1]=1;
+        for(int i=2; i<fibNumbers.length; i++){
+            fibNumbers[i]=fibNumbers[i-1]+fibNumbers[i-2];
         }
-        return k+a;
+        return fibNumbers[(int) n]%10-1;
 
     }
 
-    public static long sum(int index) {
-        int sum = 0;
-        for (int j = 0; j <= index; j++) {
-            sum=sum+arrayList.get(j);
-        }
-        return sum;
-
-    }
-
-    private static void getFibonacciHugeNaive(long n) {
-        arrayList.add(0);
-        arrayList.add(1);
-        int i=0;
-        while (i < 60) {
-            long temp = arrayList.get(i) % 10;
-            long temp1 = arrayList.get(i + 1) % 10;
-            long add = (temp + temp1) % 10;
-
-            arrayList.add((int) add);
-            i++;
-        }
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -49,4 +21,3 @@ public class FibonacciSumLastDigit {
         System.out.println(s);
     }
 }
-
